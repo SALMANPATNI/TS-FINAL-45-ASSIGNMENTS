f4872532940d251a43a2b4f16996de539c884aea
SALMAN PATNI <159817348+SALMANPATNI@users.noreply.github.com>
2024-03-15 05:20:01 -0700

    Update main.ts

--- ASSIGMENT NO 11/main.ts
+++ ASSIGMENT NO 11/main.ts
@@ -1,4 +1,4 @@
 //Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.//
 
-let friendsname = ["Yasir","Tabish", "Hasu","TEHREEM","Bushra"];
+let friendsname = ["Yasir","Tabish", "Hasu","Tehreem","Bushra"];
 friendsname.forEach(friendname =>console.log(friendname));
