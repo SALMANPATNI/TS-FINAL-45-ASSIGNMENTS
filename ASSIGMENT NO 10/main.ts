c55506b57e1a9439e3211f71567d5bff2afaeb66
SALMAN PATNI <159817348+SALMANPATNI@users.noreply.github.com>
2024-03-15 05:23:54 -0700

    Update main.ts

--- ASSIGMENT NO 10/main.ts
+++ ASSIGMENT NO 10/main.ts
@@ -1,26 +1,26 @@
 //Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
 // Name : Salman PATNI //
 // Date : 02/03/2023 //
-// addition to operations that each result in the number is 8.//
+// program for addition to operations that each result in the number is 8.//
 
 console.log(2+6);
 
 // Name : Salman PATNI //
 // Date : 02/03/2023 //
-// subtraction to operations that each result in the number is 8.//
+// program for subtraction to operations that each result in the number is 8.//
 
 console.log(14-6);
 
 
 // Name : Salman PATNI //
 // Date : 02/03/2023//
-// multipilication to operations that each result in the number is 8.//
+// For multipilication program to operations that each result in the number is 8.//
 
 console.log(2*4);
 
 
 // Name : Salman PATNI //
 // Date : 02/03/2023//
-// division to operations that each result in the number is 8.//
+// For division program to operations that each result in the number is 8.//
 
 console.log(32/4);
